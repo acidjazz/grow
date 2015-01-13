3ee0f86aa7251ba745c07759772c6fa123a331fc
kevin olson <acidjazz@gmail.com>
2015-01-12 18:24:14 -0800

    errors

--- scr/deploy.sh
+++ scr/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3outcastcfg  --exclude 'cof/*' --exclude 'sty/*' --exclude 'tpl/*' --exclude '.git/*' --exclude '.DS_Store' --exclude 'css/_*' --exclude 'scr/*' --exclude 'cfg/*' --exclude 'node_modules/*' sync . s3://weedcalculator.com/
-s3cmd setacl --config=/Users/k/.s3outcastcfg s3://weedcalculator.com/ --acl-public --recursive
+s3cmd --config=/Users/k/.s3jeffcfg  --exclude 'cof/*' --exclude 'sty/*' --exclude 'tpl/*' --exclude '.git/*' --exclude '.DS_Store' --exclude 'css/_*' --exclude 'scr/*' --exclude 'cfg/*' --exclude 'node_modules/*' sync . s3://weedcalculator.com/
+s3cmd setacl --config=/Users/k/.s3jeffcfg s3://weedcalculator.com/ --acl-public --recursive
